317acf1984cac6265ede441cb243411049c20d5d
asantos649 <asantos@alumni.nd.edu>
2020-09-10 14:27:57 -0400

    moved to card

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,13 +1,41 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+const useStyles = makeStyles({
+    root: {
+    //   minWidth: 100,
+    //   minHeight: 200,
+      width: '30%',
+      margin: '2%',
+    //   marginLeft: '5%',
+    //   marginTop: '100px',
+    },
+    title: {
+      fontSize: 14,
+      float: 'center'
+    },
+
+});
 
 function Trip(){
 
+    
+
+    const classes = useStyles();
     return(
-        <div>
-            <div>Here is a trip placeholder</div>
-            
-            
-        </div>
+        <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Actions
+          </Typography>
+         
+        </CardContent>
+        
+      </Card>
     )
 }
 
